a33cd5646c70b6ffef90dd255e39bfec0c0d33a0
Haziq Khalid <zeek@bandlab.com>
2024-01-18 15:23:08 +0800

    added omission of DS Store files

--- filenameCounterBatch/filenameCounterBatch.py
+++ filenameCounterBatch/filenameCounterBatch.py
@@ -7,7 +7,13 @@ print('This character counter script is only accurate with filenames that end wi
 #x = os.listdir('../workspace/SPROKITBEATS presents TRAPNBLAP/LOOPS')
 x = filedialog.askdirectory()
 folderName = os.listdir(x)
+try: 
+    folderName.remove('.DS_Store')
+except ValueError:
+    print('')
+
 folderNameSorted = sorted(folderName, key=len)
 
-for i in range(len(folderNameSorted)): 
+for i in range(len(folderNameSorted)):
     print('The filename contains ' + str(len(folderNameSorted[i])-4) + ' characters from [' + folderNameSorted[i] +'].')
+print('There are ' + str(len(folderNameSorted)) + ' files in this folder.')
