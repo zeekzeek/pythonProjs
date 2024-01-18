e7d5eb07ddbb733b1a693ade68f429f529d7c9e4
Haziq Khalid <zeek@bandlab.com>
2024-01-18 11:08:18 +0800

    Update filenameCounterBatch.py

--- filenameCounterBatch.py
+++ filenameCounterBatch.py
@@ -1,6 +1,12 @@
 import os
+from tkinter import filedialog
 
-x = os.listdir('../workspace/SPROKITBEATS presents TRAPNBLAP/LOOPS')
+print('This character counter script is only accurate with filenames that end with formats with 4 characters.')
 
-for i in range(len(x)): 
-    print(len(x[i])-4, x[i])
+#x = os.listdir('../workspace/SPROKITBEATS presents TRAPNBLAP/LOOPS')
+x = filedialog.askdirectory()
+folderName = os.listdir(x)
+
+for i in range(len(folderName)): 
+    print('The filename contains ' + str(len(folderName[i])-4) + ' characters from [' + folderName[i] +'].')
+    print (folderName[0])
