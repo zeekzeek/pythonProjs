4c7d511a60e4503712bd4ee527ac4097d4a78848
Haziq Khalid <zeek@bandlab.com>
2024-02-05 14:30:50 +0800

    added batch files to wavSubdirectoryCounter and filenameCounterBatch

---
+++ wavSubdirectoryCounter/wavSubdirectoryCounter.bat
@@ -0,0 +1,3 @@
+@py "C:\Users\USER\Documents\GitHub\pythonProjs\wavSubdirectoryCounter\wavSubdirectoryCounter.py" %*
+@pause
+@goto begin
---
+++ wavSubdirectoryCounter/wavSubdirectoryCounter.py
@@ -0,0 +1,16 @@
+import os
+import fnmatch
+from tkinter import filedialog
+
+def count_wav_files(directory):
+    count = 0
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if fnmatch.fnmatch(file, '*.wav'):
+                count += 1
+    return count
+
+directory = filedialog.askdirectory()
+wav_count = count_wav_files(directory)
+print(f"Number of .wav files in {directory} and its subdirectories: {wav_count}")
+print(wav_count)
