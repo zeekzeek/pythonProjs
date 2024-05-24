b5568a3a408deda0d67014fc6f188462f3e3e1e4
Haziq Khalid <zeek@bandlab.com>
2024-05-24 10:56:42 +0800

    wavSubdirectoryCounter with size

--- scEdit/scEdit.py
+++ scEdit/scEdit.py
@@ -127,6 +127,8 @@ while True:
         continue
     
     if userInput == '5':
+        directory = filedialog.askdirectory()
+        
         def count_wav_files(directory):
             count = 0
             for root, dirs, files in os.walk(directory):
@@ -138,12 +140,28 @@ while True:
                         if len(file) - 4 >= 50 :
                             print(file + " [" + str(len(file) - 4) + f" characters]. Please shorten the filename to less than 50. ~Size: {sizeText:.2f}mb~")
                         else:
-                            print(file + " [" + str(len(file) - 4) + " characters].")
+                            print(file + " [" + str(len(file) - 4) + f" characters]. ~Size: {sizeText:.2f}mb~")
             return count
 
-        directory = filedialog.askdirectory()
+        def get_size(start_path = '.'):
+            total_size = 0
+            for dirpath, dirnames, filenames in os.walk(directory):
+                for f in filenames:
+                    fp = os.path.join(dirpath, f)
+                    # skip if it is symbolic link
+                    if not os.path.islink(fp):
+                        total_size += os.path.getsize(fp)
+
+            return total_size
+
+        getSize = get_size() / 1000000
         wav_count = count_wav_files(directory)
-        print(f"Number of .wav files in {directory} and its subdirectories: {wav_count}\n\n")
+        print(f"\nNumber of .wav files in {directory} and its subdirectories: {wav_count}\nTotal size: {getSize:.2f}mb\n")
+
+        if getSize > 2000:
+            print('SIZE NEEDS TO BE REDUCED TO UNDER 2GB.\n')
+        else:
+            pass
         continue
 
     if userInput == '6':
