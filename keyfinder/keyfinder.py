4b6d5717f35bb1a7a54a4ff63064bb165c2fad22
Haziq Khalid <zeek@bandlab.com>
2024-09-02 11:18:14 +0800

    new keyfinder script

---
+++ keyfinder/keyfinder.py
@@ -0,0 +1,28 @@
+#requires installation of pymusickit library
+#source: https://pypi.org/project/pymusickit/
+
+import os
+import fnmatch
+from tkinter import filedialog
+from pymusickit.key_finder import KeyFinder
+window = filedialog.Tk()
+window.wm_attributes('-topmost', 1)
+window.withdraw()   # this supress the tk window
+
+directory = filedialog.askdirectory()
+
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        if fnmatch.fnmatch(file, '*.wav'):
+            fullpath = os.path.join(root,file)
+            print(file)
+            audio_path = fullpath
+            song = KeyFinder(audio_path)
+            song.print_key()
+            print('====================')
+            
+#print(directory)
+#audio_path='DirtyClubElectrocumbia_3_100_Am_Synths_01_4bars.wav'
+#song = KeyFinder(audio_path)
+
+#song.print_key()
