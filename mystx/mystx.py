0aa8fefd94b80e97bbc9205ea4f8ad0baf9b1a4d
Haziq Khalid <zeek@bandlab.com>
2024-10-02 13:44:51 +0800

    mystx

---
+++ mystx/mystx.py
@@ -0,0 +1,9 @@
+import webbrowser
+
+url1 = "https://www.bandlab.com/sounds/pack/mystxrivl-wave-starter"
+url2 = "https://www.bandlab.com/sounds/pack/mystxrivl-wave-starter-pack-vol-2"
+url3 = "https://www.bandlab.com/sounds/pack/mystxrivl-wave-starter-pack-vol-3"
+
+webbrowser.open(url1)
+webbrowser.open(url2) 
+webbrowser.open(url3)
